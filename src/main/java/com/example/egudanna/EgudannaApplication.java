8f25d830a9f9dc16267ff3aa1754bea0f8a8b5ac
haeunNoh06 <okhagrace06@naver.com>
2024-03-24 23:08:37 +0900

    :sparkles: [feat] springboot server init

---
+++ src/main/java/com/example/egudanna/EgudannaApplication.java
@@ -0,0 +1,13 @@
+package com.example.egudanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgudannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgudannaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/egudanna/EgudannaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egudanna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgudannaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
