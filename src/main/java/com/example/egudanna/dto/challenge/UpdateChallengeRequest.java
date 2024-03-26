dd37cdb3ba21884be911c55ea1cf99d8861c8e53
haeunNoh06 <okhagrace06@naver.com>
2024-03-26 09:34:16 +0900

    :sparkles: [feat] challenge UpdateChallengeRequest

---
+++ src/main/java/com/example/egudanna/dto/challenge/UpdateChallengeRequest.java
@@ -0,0 +1,20 @@
+package com.example.egudanna.dto.challenge;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UpdateChallengeRequest {
+    private Long id;
+    private Long videoId;
+    private Long likeNum;
+    private Long levelId;
+    private String title;
+    private String nickname;
+    private String hashtag;
+    private String email;
+    private String password;
+}
