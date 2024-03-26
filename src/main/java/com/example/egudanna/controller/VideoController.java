6dcf4b9a65aa0ae14e3e7c7782825bac460852e2
haeunNoh06 <okhagrace06@naver.com>
2024-03-26 13:03:43 +0900

    :sparkles: [feat] video get api 완성

--- src/main/java/com/example/egudanna/controller/VideoController.java
+++ src/main/java/com/example/egudanna/controller/VideoController.java
@@ -2,14 +2,15 @@ package com.example.egudanna.controller;
 
 import com.example.egudanna.domain.Video;
 import com.example.egudanna.dto.video.AddVideoRequest;
+import com.example.egudanna.dto.video.VideoResponse;
 import com.example.egudanna.service.VideoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +24,24 @@ public class VideoController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedVideo);
+    }
+
+    @GetMapping("/api/videos")
+    public ResponseEntity<List<VideoResponse>> findAllVideos() {
+        List<VideoResponse> videos = videoService.findAll()
+                .stream()
+                .map(VideoResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(videos);
+    }
+
+    @GetMapping("/api/videos/{id}")
+    public ResponseEntity<VideoResponse> findVideo(@PathVariable("id") Long id) {
+        Video video = videoService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new VideoResponse(video));
     }
 }
