764362ab8ac44c2504e7bd8052474f87fb37d59a
haeunNoh06 <okhagrace06@naver.com>
2024-03-26 09:09:13 +0900

    :sparkles: [feat] challenge service findAll, findById

--- src/main/java/com/example/egudanna/service/ChallengeService.java
+++ src/main/java/com/example/egudanna/service/ChallengeService.java
@@ -2,9 +2,12 @@ package com.example.egudanna.service;
 
 import com.example.egudanna.domain.Challenge;
 import com.example.egudanna.dto.challenge.AddChallengeRequest;
+import com.example.egudanna.dto.challenge.ChallengeResponse;
 import com.example.egudanna.repository.ChallengeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +16,14 @@ public class ChallengeService {
 
     public Challenge save(AddChallengeRequest request) {
         return challengeRepository.save(request.toEntity());
+    }
+
+    public List<Challenge> findAll() {
+        return challengeRepository.findAll();
+    }
+
+    public Challenge findById(Long id) {
+        return challengeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found "+id));
     }
 }
