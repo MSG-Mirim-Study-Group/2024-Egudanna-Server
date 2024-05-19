2df15a371f7d9b97e9be7b97bbf911893e2e6807
huihyeon06 <113982023+huihyeon06@users.noreply.github.com>
2024-05-20 01:40:24 +0900

    [feat] : group의 post, get 작성

---
+++ src/main/java/com/example/egudanna/controller/GroupController.java
@@ -0,0 +1,40 @@
+package com.example.egudanna.controller;
+
+import com.example.egudanna.domain.Group;
+import com.example.egudanna.dto.Group.GroupResponse;
+import com.example.egudanna.repository.GroupRepository;
+import com.example.egudanna.service.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/group")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    @PostMapping
+    public Group createGroup(@RequestBody GroupResponse groupResponse) {
+        return groupService.createGroup(groupResponse);
+    }
+
+    @GetMapping
+    public List<Group> getAllGroups() {
+        return groupService.getAllGroups();
+    }
+
+    @GetMapping("/{group_id}")
+    public List<Object[]> getCategoryGenreByGroupId(@PathVariable("group_id") long group_id) {
+        return groupService.getCategoryGenreByGroupId(group_id);
+    }
+
+    @DeleteMapping("/{group_id}")
+    public void deleteGroup(@PathVariable("group_id") long group_id) {
+
+    }
+
+}
