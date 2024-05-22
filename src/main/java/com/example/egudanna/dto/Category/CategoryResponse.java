9fe8f4aed0f9867a2077f40ce5c4d281e50b4753
huihyeon06 <113982023+huihyeon06@users.noreply.github.com>
2024-05-22 16:27:10 +0900

    [feat] : category 데이터 가져오는 api 구현

---
+++ src/main/java/com/example/egudanna/dto/Category/CategoryResponse.java
@@ -0,0 +1,12 @@
+package com.example.egudanna.dto.Category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponse {
+    private String genre;
+}
