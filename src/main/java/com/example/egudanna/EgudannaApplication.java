127fd1ddfdcdcf37ad292407d1e32c195d7752a9
haeunNoh06 <okhagrace06@naver.com>
2024-05-27 14:53:39 +0900

    :sparkles: [feat] gmail sent image file successfully

--- src/main/java/com/example/egudanna/EgudannaApplication.java
+++ src/main/java/com/example/egudanna/EgudannaApplication.java
@@ -1,14 +1,8 @@
 package com.example.egudanna;
 
-import com.example.egudanna.email.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
@@ -17,17 +11,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 //@EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class })
 public class EgudannaApplication {
-	@Autowired
-	private EmailSenderService senderService;
+//	@Autowired
+//	private EmailSenderService senderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgudannaApplication.class, args);
 	}
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void sendMail() {
-		senderService.sendMail("okhagrace0610@gmail.com",
-				"This is Subject",
-				"This is Body of Email");
-	}
+//	@EventListener(ApplicationReadyEvent.class)
+//	public void sendMail() {
+//		senderService.sendMail("s2216@e-mirim.hs.kr", // 받는 사람의 이메일
+//				"지현아 안녕",
+//				"This is Body of Email");
+//	}
 }
